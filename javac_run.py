a3f31ca69246fcda42a807e94ed4cf14c9b07769
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-11-03 00:38:38 +0530

    Update javac_run.py

--- javac_run.py
+++ javac_run.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2024 Amalnath
+Copyright (c) 2024 MrTG-CodeBot
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
